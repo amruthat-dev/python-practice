096e49861d28db1ecae802b7f32374b8b9bbed36
Amrutha T <amruthat730@gmail.com>
2026-08-03 21:12:08 +0530

    Add while loop practice

---
+++ day_7/while_loop.py
@@ -0,0 +1,25 @@
+is_failed = True
+i = 1
+while is_failed and i<=100:
+    print(f"try {i}")
+    i += 1
+
+print("stop here")
+
+
+
+# 1. Start
+i = 1
+
+# 2. Condition
+while i <= 5:
+
+    print(i)
+    i += 1
+
+
+    i = 1
+
+while i <= 5 and i<=10:
+    print(i)
+    i += 1
