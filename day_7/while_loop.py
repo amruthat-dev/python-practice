c34680d8421777ffa5f88279ad038b972eca795c
Amrutha T <amruthat730@gmail.com>
2026-08-04 20:55:28 +0530

    update while loop practice file

--- day_7/while_loop.py
+++ day_7/while_loop.py
@@ -1,25 +1,35 @@
-is_failed = True
 i = 1
-while is_failed and i<=100:
-    print(f"try {i}")
-    i += 1
+while i <=5:
+    print(i)
+    i +=1
 
-print("stop here")
-
-
-
-# 1. Start
 i = 1
-
-# 2. Condition
-while i <= 5:
-
+while i <= 10:
     print(i)
     i += 1
 
-
-    i = 1
-
-while i <= 5 and i<=10:
+i = 10
+while i >= 1:
     print(i)
+    i -= 1
+
+i = 2
+while i <= 20:
+    print(i)
+    i += 2
+
+i = 1
+while i <= 19:
+    print(i)
+    i += 2
+
+i = int(input("Enter a number: "))
+while i <= 5 and i >= 1:
+    print(i)
+    i -= 1
+
+number = int(input("Enter a number: "))
+i = 1
+while i <= 10:
+    print(f"{number} x {i} = {number * i}")
     i += 1
