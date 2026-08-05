afab6852eea95e584efe98d676dd2619268be992
Amrutha T <amruthat730@gmail.com>
2026-08-05 15:58:08 +0530

    adding sum and factorial program

--- day_7/while_loop.py
+++ day_7/while_loop.py
@@ -28,8 +28,31 @@ while i <= 5 and i >= 1:
     print(i)
     i -= 1
 
+    
+#multiplication table of a number using while loop
 number = int(input("Enter a number: "))
 i = 1
 while i <= 10:
     print(f"{number} x {i} = {number * i}")
     i += 1
+
+
+#sum of first 10 natural numbers using while loop
+total = 0
+i = 1
+while i <= 10:
+    total += i
+    i += 1
+
+print(f"Sum of first 10 natural numbers is: {total}")
+
+
+#factorial of a number using while loop
+number = int(input("Enter a number: "))
+factorial = 1
+i =1
+while i<= number:
+    factorial *= i
+    i += 1
+
+print(f"Factorial of {number} is: {factorial}")
