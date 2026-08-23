73273712f2662ae212a2018fdd7cce7fda016fd2
Amrutha T <amruthat730@gmail.com>
2026-08-23 13:41:40 +0530

    update nested loop

--- day_7/nested_while_loop.py
+++ day_7/nested_while_loop.py
@@ -5,3 +5,12 @@ while i <= 10:
         print("Amrutha")
         x += 1
     i += 1
+
+pin = " "
+correct_pin = "1234"
+while pin != correct_pin:
+    pin = input("Enter your pin: ")
+    if pin != correct_pin:
+        print("incorrect pin! Try again")
+        
+print("correct pin")
