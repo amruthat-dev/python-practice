01c609f482edbb157cedd589862f63975fc26271
Amrutha T <amruthat730@gmail.com>
2026-09-02 21:26:28 +0530

    add sum of list using for loop

---
+++ day_9/loops_adv.py
@@ -0,0 +1,9 @@
+#looping through a list
+#sum of all numbers in a list 
+list = [23, 45, 67, 89, 12]
+total = 0
+for num in list:
+    total += num
+
+
+print(f"Sum of all numbers in the list is: {total}")
